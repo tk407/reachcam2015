e5cbbcdb6c5071abede907b1820f39c8f1100c66
Tamas Kispeter <tk407@cam.ac.uk>
2015-07-14 12:30:34 +0100

    Revert "Tamas: Changed background to blue"
    This reverts commit 085af58c8c627c4e0c155d05a05001433aa222a0.

--- src/RacingGame.java
+++ src/RacingGame.java
@@ -48,7 +48,7 @@ public class RacingGame extends Applet implements Runnable {
 		track = new Track();
 		//track.sections = sections;
 		setFont( font );
-		setBackground( Color.blue );
+		setBackground( Color.white );
 	}
 
 	public RacingGame( byte id, Vehicle[] vehicles, NetworkModule module ) {
